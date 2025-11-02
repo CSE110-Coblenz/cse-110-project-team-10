b6bf9ab1287ec3244a387fbf69cb5f09e31b6d1e
aidan <aidan@aidanjinlee.com>
2025-11-01 18:58:49 -0700

    added game physics and basic logic

---
+++ src/client/gameplay/Physics.ts
@@ -0,0 +1,18 @@
+import { Position, ShotParams } from './types.ts'; 
+
+const gravity = 9.81 *1.5; 
+
+
+// Calculates the (x, y) position of the ball at a specific time after launch.
+export function calculatePositionAtTime(params: ShotParams, time: number): Position {
+	const { angle, velocity } = params; 
+	const angleRadians = angle * (Math.PI/180); 
+	
+	const vx = velocity * Math.cos(angleRadians);
+	const vy = velocity * Math.sin(angleRadians); 
+
+	const x = vx * time; 
+	const y = (vy * time) - (0.5 * gravity * Math.pow(time, 2)); 
+
+	return { x, y }; 
+}
---
+++ src/client/gameplay/types.ts
@@ -0,0 +1,15 @@
+// Defining data to be used
+
+export interface Position { 
+	x: number;
+	y: number; 
+}
+
+
+export interface ShotParams {
+	// angle in degrees
+	angle: number; 
+	// initial velocity in m/s 
+	velocity: number; 
+
+}	
